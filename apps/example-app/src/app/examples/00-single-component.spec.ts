009d795cd91a5ff23a1215dd50c793becf7bfed5
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-10-14 20:08:02 +0200

    reproduce jest example

--- apps/example-app/src/app/examples/00-single-component.spec.ts
+++ apps/example-app/src/app/examples/00-single-component.spec.ts
@@ -2,9 +2,28 @@ import { render, screen, fireEvent } from '@testing-library/angular';
 
 import { SingleComponent } from './00-single-component';
 
-test('renders the current value and can increment and decrement', async () => {
-  await render(SingleComponent);
+beforeEach(async () => {
+  await render('<app-fixture></app-fixture>', {
+    declarations: [SingleComponent],
+  });
+});
 
+test('renders the current value and can increment and decrement', async () => {
+  const incrementControl = screen.getByRole('button', { name: /increment/i });
+  const decrementControl = screen.getByRole('button', { name: /decrement/i });
+  const valueControl = screen.getByTestId('value');
+
+  expect(valueControl).toHaveTextContent('0');
+
+  fireEvent.click(incrementControl);
+  fireEvent.click(incrementControl);
+  expect(valueControl).toHaveTextContent('2');
+
+  fireEvent.click(decrementControl);
+  expect(valueControl).toHaveTextContent('1');
+});
+
+test('renders the current value and can increment and decrement 2', async () => {
   const incrementControl = screen.getByRole('button', { name: /increment/i });
   const decrementControl = screen.getByRole('button', { name: /decrement/i });
   const valueControl = screen.getByTestId('value');
--- apps/example-app/src/test-setup.ts
+++ apps/example-app/src/test-setup.ts
@@ -1,7 +1,17 @@
 import 'jest-preset-angular/setup-jest';
 import '@testing-library/jest-dom';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
 import { configure } from '@testing-library/angular';
 import { ReactiveFormsModule } from '@angular/forms';
+
+getTestBed().resetTestEnvironment();
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+  { teardown: { destroyAfterEach: true } }, // 👈
+);
 
 configure({
   defaultImports: [ReactiveFormsModule],
